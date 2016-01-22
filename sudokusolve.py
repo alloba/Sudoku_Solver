b42986d572cd56949267145d8c1cedcd634ed040
Alex <alexlbates@gmail.com>
2016-01-22 18:31:44 -0500

    general UI changes.
    added a menu to open new puzzles, made the enter key step through the puzzle, shifted some code around to make it better to read.

--- sudokusolve.py
+++ sudokusolve.py
@@ -1,11 +1,13 @@
 import tkinter
 from puzzlecontainer import SudokuPuzzle
+from tkinter import filedialog
 
 def gatherinput(filename):
     """
      take input from a file, with directory specified as a parameter.
      ideally, correct format should be "[num][space][num][space].....[num][newline] * 9"
 
+    currently this is just used as an initialization for the gui, to fill it with something at the start
     """
 
     file = open(filename)
@@ -15,39 +17,12 @@ def gatherinput(filename):
     return listify
 
 
-# Finally, the bit that executes, after all those functions.
 puzzle = SudokuPuzzle(gatherinput("C:\\CourseWork\\AI\\Sudoku\\sudoku.txt"))
 puzzleprevstep = puzzle.puzzle[:]
 
-# GUI Setup
-window = tkinter.Tk()
-window.title("Sudoku Solver")
-
-bool_m1 = tkinter.BooleanVar(window, False)
-bool_m2 = tkinter.BooleanVar(window, False)
-
-entrylist = []
-for i in range(81):
-    entrylist.append(tkinter.Entry(window, width=5))
-    entrylist[i].insert(0, puzzle.puzzle[i])
-
-for i in range(9):
-    for j in range(9):
-        entrylist[i*9 + j].grid(row=i, column=j+1)
-
-validtext = tkinter.StringVar(window, " - ")
-lbl_validmarker = tkinter.Label(window, textvariable=validtext)
-lbl_validmarker.grid(row=13, column=0)
-
-lbl_solutionsdisplay = tkinter.Label(window, text="Found: ")
-lbl_solutionsdisplay.grid(row=12, column=0)
-
-solutionsfound = tkinter.IntVar(window, 0)
-lbl_solutionsfound = tkinter.Label(window, textvariable=solutionsfound)
-lbl_solutionsfound.grid(row=12, column=1)
 
 # GUI Methods
-def solveStep():
+def solveStep(junkvar=0):  # try to solve one step in the puzzle, and display an update.
     puzzle.updatepuzzle(getEntries())
 
     simplebool = False
@@ -73,14 +48,17 @@ def solveStep():
         isValid()
 
 
-# GUI Methods
 def updateEntries():
     for i in range(81):
         if puzzle.puzzle[i] != puzzleprevstep[i]:
             entrylist[i].delete(0, tkinter.END)
             entrylist[i].insert(0, str(puzzle.puzzle[i]))
-            entrylist[i].config(bg="red")
+            entrylist[i].config(bg="red")  # to show that something has been filled in here
 
+def setEntries():
+    for i in range(81):
+        entrylist[i].delete(0, tkinter.END)
+        entrylist[i].insert(0, str(puzzle.puzzle[i]))
 
 def getEntries():
     puzzleString = []
@@ -91,17 +69,68 @@ def getEntries():
 
 def isValid():
     if puzzle.validatepuzzle():
-        validtext.set("Valid")
+        validtext.set("Solved")
     else:
-        validtext.set("Not Valid")
+        validtext.set("Not Solved")
+
+
+def openfile():
+    filename = filedialog.askopenfilename(parent=window)
+    try:
+        global puzzleprevstep
+        puzzle.updatepuzzle(gatherinput(filename))
+        puzzleprevstep = puzzle.puzzle[:]
+        setEntries()
+        for entry in entrylist:
+            entry.config(bg="white")
+    except (FileNotFoundError, UnicodeDecodeError, ValueError) as e:
+        return
+
+# GUI Setup
+window = tkinter.Tk()
+window.resizable(width=0, height=0)
+window.title("Sudoku Solver")
+
+# these bools are for the checkboxes that select methods
+bool_m1 = tkinter.BooleanVar(window, True)
+bool_m2 = tkinter.BooleanVar(window, False)
+
+# all the boxes that display the puzzle, put into a group to make it display better
+entrygroup = tkinter.LabelFrame(window, text="")
+entrygroup.grid(row=0, column=0, rowspan=9, columnspan=9, padx=40)
+entrylist = []
+for i in range(81):
+    entrylist.append(tkinter.Entry(entrygroup, width=3, justify=tkinter.CENTER))
+    entrylist[i].insert(0, puzzle.puzzle[i])
+
+for i in range(9):
+    for j in range(9):
+        entrylist[i*9 + j].grid(row=i, column=j+1)
+
+
+validtext = tkinter.StringVar(window, "              ")
+lbl_validmarker = tkinter.Label(window, textvariable=validtext)
+lbl_validmarker.grid(row=9, column=2)
+
+lbl_solutionsdisplay = tkinter.Label(window, text="Found:")
+lbl_solutionsdisplay.grid(row=10, column=0, sticky="W")
+
+solutionsfound = tkinter.IntVar(window, 0)
+lbl_solutionsfound = tkinter.Label(window, textvariable=solutionsfound)
+lbl_solutionsfound.grid(row=10, column=1, sticky="W")
 
 chkbtn_method1 = tkinter.Checkbutton(window, text="Method 1", variable=bool_m1, onvalue=True, offvalue=False)
 chkbtn_method2 = tkinter.Checkbutton(window, text="Method 2", variable=bool_m2, onvalue=True, offvalue=False)
-chkbtn_method1.grid( row=10, column=0)
-chkbtn_method2.grid( row=11, column=0)
+chkbtn_method1.grid(row=9, column=0)
+chkbtn_method2.grid(row=9, column=1)
 
 btn_execute = tkinter.Button(window, text="Step", command=solveStep)
-btn_execute.grid(row=0, column=0)
+btn_execute.grid(row=9, column=8)
 
+menubar = tkinter.Menu(window)
+menubar.add_command(label="Open New Puzzle", command=openfile)
+
+window.bind('<Return>', solveStep)
+window.config(menu=menubar)
 window.mainloop()
 
